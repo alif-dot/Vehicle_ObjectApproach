6ea4cf34688aa714da6066b54f35aed1efd4cb0b
alif-dot <alif17648@gmail.com>
2023-05-09 23:14:56 +0600

    Add project files.

---
+++ Vehicle_ObjectApproach/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_ObjectApproach
+{
+    sealed class Motorcycle : TwoWheeler
+    {
+        public Motorcycle()
+        {
+            vehicleTypes = VehicleType.TwoWheeler;
+            NumberOfSeat = 2;
+        }
+        public decimal BPH { get; set; }
+        public decimal KMPL { get; set; }
+        public decimal Cooling { get; set; }
+    }
+}
---
+++ Vehicle_ObjectApproach/VehicleType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_ObjectApproach
+{
+    enum VehicleType
+    {
+        TwoWheeler = 2,
+        ThreeWheeler = 3,
+        FourWheeler = 4
+    }
+
+}
